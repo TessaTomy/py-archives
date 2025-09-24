1c0a920fc7adc0041396f4d768ffff6676af63a4
Tessa Tomy <tessatomy757@gmail.com>
2025-09-24 20:13:30 +0530

    if...elif..else implementation

---
+++ grades_pg15.py
@@ -0,0 +1,9 @@
+pre=int(input("Enter Your percentage :"))
+if pre>90:
+    print("Grade : A")
+elif pre>80 :
+    print("Grade : B")
+elif pre>70:
+    print("Grade : C")
+else:
+    print("Grade : D")
