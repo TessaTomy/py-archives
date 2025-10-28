c6a5734fdd9b21528ea588abafc8fa21668c3498
Tessa Tomy <tessatomy757@gmail.com>
2025-10-28 19:19:03 +0530

    Add demo_graphics_package.py to test package imports and usage

---
+++ demo_graphics_package.py
@@ -0,0 +1,14 @@
+from graphics import *
+from graphics.graphics_3d import *
+from graphics.circle import *
+
+print("Rectangle Area:", rectangle.area(10, 5))
+print("Rectangle Perimeter:", rectangle.perimeter(10, 5))
+
+print("Circle Area:", area(7))
+print("Circle Perimeter:", perimeter(7))
+
+print("Cuboid Area:", cuboid.area(4, 3, 2))    
+print("Cuboid Perimeter:", cuboid.perimeter(4, 3, 2))
+
+print("Sphere Perimeter:", sphere.perimeter(6))
